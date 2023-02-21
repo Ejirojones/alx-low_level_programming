ae8919ee00a342b648fb9e43d4981d0bb8bc0951
ejiro <ejirojones2@gmail.com>
2023-02-21 13:31:50 -0800

    LAU

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - letter, lower or alphabets
+ * @c: parameter
+ * Return: 0 0r 1
+ */
+
+int _isalpha(int c)
+{
+	if (c <= 127)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
