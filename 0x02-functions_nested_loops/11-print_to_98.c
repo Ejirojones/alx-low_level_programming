fa7a06cd9dde80028baaca6231f695ed64a82225
ejiro <ejirojones2@gmail.com>
2023-02-21 19:11:33 -0800

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,50 +9,40 @@
 
 void print_to_98(int n)
 {
-	int i;
+int i;
 
-	if (n >= 0 && n <= 98)
-	{
-		for (i = n ; i <= 98 ; i++)
-		{
-			printf("%d", i);
-			if (i != 98)
-			{
-				printf(", ");
-			}
-
-		}
-		printf("\n");
-	}
-	else if (n < 0)
-	{
-		for (i = n ; i <= 98 ; i++)
-		{
-			printf("%d", i);
-			if (i != 98)
-                        {
-                                printf(", ");
-                        }
-		}
-		printf("\n");
-	}
-	else if (n > 98)
-	{
-		for (i = n ; i >= 98 ; i--)
-		{
-			printf("%d", i);
-			if (i != 98)
-                        {
-                                printf(", ");
-                        }
-		}
-		printf("\n");
-	}
-	else if (n == 98)
-	{
-		printf("%d", n);
-	}
-	
-
-
+if (n >= 0 && n <= 98)
+{
+for (i = n ; i <= 98 ; i++)
+{
+printf("%d", i);
+if (i != 98)
+printf(", ");
+}
+printf("\n");
+}
+else if (n < 0)
+{
+for (i = n ; i <= 98 ; i++)
+{
+printf("%d", i);
+if (i != 98)
+printf(", ");
+}
+printf("\n");
+}
+else if (n > 98)
+{
+for (i = n ; i >= 98 ; i--)
+{
+printf("%d", i);
+if (i != 98)
+printf(", ");
+}
+printf("\n");
+}
+else if (n == 98)
+{
+printf("%d", n);
+}
 }
