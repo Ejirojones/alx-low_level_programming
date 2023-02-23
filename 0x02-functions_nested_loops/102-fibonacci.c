114a17cd5016487affd257d43125a93872820ba0
ejiro <ejirojones2@gmail.com>
2023-02-22 16:21:47 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - fibonacci no
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int sum;
+
+	sum = 0
+
+for (i = 1 ; i <= 12586269025 ; i++)
+{
+	sum = sum + i
+
+}
+printf("%d\n", sum);
+printf(',');
+printf(' ');
+}
