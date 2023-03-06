cf2014c1b1542e156f99532f7b3d060810749660
ejiro <ejirojones2@gmail.com>
2023-03-06 14:24:32 -0800

    chessboard

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strstr - function that locates a substring
+ * @haystack: input value
+ * @needle: input value
+ * Return: 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+}
